418898004f2141ed6a6a4d07b9c566a037229a49
qianhao_ji <qj2030@nyu.edu>
2023-04-11 18:05:36 -0400

    g

---
+++ ass8/DapperTutorialMarch/DapperTutorialMarch/Menu/Menu.cs
@@ -0,0 +1,55 @@
+﻿using Infrastructure.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperTutorialMarch.Menu
+{
+    public class MenuSelection
+    {
+        DepartmentService e;
+        
+        public MenuSelection() 
+        {
+            e = new DepartmentService();
+        }
+        public void Run()
+        {
+            int choice = this.InputChoice();
+            if(choice <=0 || choice>5)
+            {
+                Console.WriteLine("please give a integer in range [1,5]");
+            }
+            if(choice == 1)
+            {
+                e.AddDepartment();
+            }
+            else if(choice == 2)
+            {
+                e.DeleteDepartment();
+            }
+            else if(choice == 3)
+            {
+                e.GetAllDepartments();
+            }
+            else if (choice == 4)
+            {
+                e.UpdateDepartment();
+            }
+            else
+            {
+                e.GetDepartmentById();
+            }
+
+        }
+
+        public int InputChoice()
+        {
+            int choice = int.Parse(Console.ReadLine());
+            
+            return choice;
+        }
+    }
+}
